919769756a4012f1be3e098929d8cbfaf5587186
BasbustDama <aleksandra.200328@gmail.com>
2022-03-19 00:38:43 +0300

    add category card button

---
+++ lib/internal/text_styles.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const optionStyle =
+    TextStyle(fontSize: 14, fontWeight: FontWeight.w500, color: Colors.white);
---
+++ lib/presentation/widgets/category_card.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:oggetto_afisha_front/internal/text_styles.dart';
+
+class CategoryCard extends StatelessWidget {
+  final String asset;
+  final String title;
+  final VoidCallback onTap;
+
+  const CategoryCard({
+    Key? key,
+    required this.asset,
+    required this.title,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      child: Ink(
+        height: 80,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: const Color(0xFF1F1F1F),
+        ),
+        padding: const EdgeInsets.only(left: 10, right: 50),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SvgPicture.network(asset),
+            const SizedBox(height: 10),
+            Text(
+              title,
+              style: optionStyle,
+            ),
+          ],
+        ),
+      ),
+      onTap: onTap,
+    );
+  }
+}
